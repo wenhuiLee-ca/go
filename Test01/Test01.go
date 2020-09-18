e28ce7b4058e6cc252edbdccadd82f33190f78fc
wenhuiLee-ca <70797889+wenhuiLee-ca@users.noreply.github.com>
2020-09-18 16:41:24 +0800

    qww

---
+++ Test01/Test01.go
@@ -0,0 +1,18 @@
+package main
+
+/**
+ * 类描述：
+ *
+ * @ClassName Test01
+ * @Description TODO
+ * @Author liwenhui
+ * @Date 2020/9/18 16:30
+ * @Version 1.0
+ */
+import (
+	"fmt"
+	_ "fmt"
+)
+func main() {
+	fmt.Println("hello world")
+}
